471d725fc771079c70c3f7ee7fa5cba4515a84ec
josé david bohoquez <jose.bohorquezn.col@upb.edu.co>
2024-02-02 17:11:24 -0500

    se modifico y se corrigieron errores del codigo fuente

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
  1/02/2024 empeze con la evaluacion de git cambiando el codigo fuente y haciendo el primer commit para mandarlo al repositorio
 
+ ![texto](fotdecodig.jpeg)
 
- ![texto](fot de codigo.jpeg)
+ [top 10 builds de darksouls3 para pvp](https://www.thegamer.com/dark-souls-3-best-pve-builds/)
--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    if   if (7)
+    if(argc < 2)
     {
         /* code */
-    }
-     (argc < 2) {
+
         fprintf(stderr,"Usage: program <number>\n");
         return 1;
     }
@@ -14,13 +13,41 @@ int main(int argc, char *argv[]) {
     int number = atoi(argv[1]); //Convert the console input argument to an integer
     
     /*It must be verified that the entered number is greater than or equal to zero*/
-    
+     if(number < 0)
+    {
+        perror("El núnmero debe ser mayor a cero");
+        exit(1);
+    }
     /*Implement the function that calculates the factorial of the input argument*/
+        int factorial = 1;
+    int b;
+    for (b = number; b > 1; b--)
+    {
+        factorial *= b;
+    }
     
     /*Print the result*/
-    
+    printf("El factorial de %d es %d",number,factorial);
     /*Implement the function that calculates if the argument is a prime number*/
-
+int count = 0,index = 1;
+    while (index < number)
+    {
+        if(number%index == 0)
+            {
+            count++;
+            }
+        index++;
+    }
+    
+    /*Print the result*/
+    if(count == 1)
+    {
+        printf("%d es número primo\n",number);
+    }
+    else
+    {
+        printf("%d no es número primo\n",number);
+    }
     /*Print the result*/
     
     return 0;
